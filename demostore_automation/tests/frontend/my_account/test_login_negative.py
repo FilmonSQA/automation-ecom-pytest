241ad7b79a6a2f8055b1972baf314ba658dc474b
Filmon <filmonticho@gmail.com>
2024-10-15 22:59:57 -0700

    Created FE test for login non exisiting user

--- demostore_automation/tests/frontend/my_account/test_login_negative.py
+++ demostore_automation/tests/frontend/my_account/test_login_negative.py
@@ -2,19 +2,21 @@
 import pytest
 from demostore_automation.src.pages.MyAccountSignedOutPage import MyAccountSignedOutPage
 
-pytestmark = [pytest.mark.feregression, pytest.mark.fesmoke, pytest.mark.my_account]
-
+pytestmark = [pytest.mark.fe, pytest.mark.regression, pytest.mark.smoke, pytest.mark.my_account]
 
 @pytest.mark.usefixtures("init_driver")
 class TestLoginNegative:
+    """
+    A set of negative tests to verify the login functionality with non-existing users.
+    """
 
     @pytest.mark.estcf13
-    @pytest.mark.estcf1
-    @pytest.mark.tcid12
-    @pytest.mark.pioneertcid1
     def test_login_none_existing_user(self):
+        """
+        Test to verify the login functionality with a non-existing user.
+        """
+        
         print("Testing None Existing User Login")
-
         my_acct_page = MyAccountSignedOutPage(self.driver)
         my_acct_page.go_to_my_account()
         my_acct_page.input_login_username("abcdef@supersqa.com")
@@ -22,9 +24,11 @@ class TestLoginNegative:
         my_acct_page.click_login_button()
 
         expected_err = "Unknown email address. Check again or try your username."
+
+        # Waiting for the error to appear
         my_acct_page.wait_until_error_is_displayed(expected_err)
 
-        # breakpoint()
+        # Adding an assertion to verify the error message is displayed correctly
+        assert my_acct_page.get_error_message() == expected_err, "Error message does not match expected text."
 
-
-
+        breakpoint()
